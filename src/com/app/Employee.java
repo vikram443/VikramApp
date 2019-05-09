97d2c6e7371f4fce9831c5d7c17ea42ca7206f59
user <user@Vikram>
2019-05-09 15:34:12 +0530

    New file is created

---
+++ src/com/app/Employee.java
@@ -0,0 +1,16 @@
+package com.app;
+
+public class Employee {
+	
+	private int empId;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	
+
+}
